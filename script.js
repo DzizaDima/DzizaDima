54205adba22c917bbee305034d1837fdeda7daed
DzizaDima <32745918+DzizaDima@users.noreply.github.com>
2019-05-21 12:23:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ function computeTotalDistance(result) {
     document.getElementById('total').innerHTML = total;
   
 }
-    data = '[{"name":"Шара (30 - 40)","number":"30 - 40","type":[{"name":"Эконом","firstkmprice":"25","otherkm":"8"},{"name":"Стандарт","firstkmprice":"49","otherkm":"9"},{"name":"Комфорт","firstkmprice":"59","otherkm":"10"},{"name":"Бизнеc","firstkmprice":"69","otherkm":"10"}],"services":[{"name":"animal","cost":"50"},{"name":"delivery","cost":"10"}]},{"name":"Maxim","number":"75 - 55","type":[{"name":"Эконом","firstkmprice":"35","otherkm":"6"},{"name":"Стандарт","firstkmprice":"-","otherkm":"-"},{"name":"Комфорт","firstkmprice":"50","otherkm":"6.5"},{"name":"Бизнеc","firstkmprice":"50","otherkm":"7.5"}],"services":[{"name":"animal","cost":"10"},{"name":"delivery","cost":"10"}]},{"name":"OnTaxi","number":"2000","type":[{"name":"Эконом","firstkmprice":"29","otherkm":"6"},{"name":"Стандарт","firstkmprice":"45","otherkm":"7"},{"name":"Комфорт","firstkmprice":"55","otherkm":"8"},{"name":"Бизнеc","firstkmprice":"65","otherkm":"11"}],"services":[{"name":"animal","cost":"Infinity"},{"name":"delivery","cost":"Infinity"}]},{"name":"Shark","number":"3000","type":[{"name":"Эконом","firstkmprice":"38.25","otherkm":"5.95"},{"name":"Стандарт","firstkmprice":"45","otherkm":"7"},{"name":"Комфорт","firstkmprice":"58.5","otherkm":"9.1"},{"name":"-","firstkmprice":"-","otherkm":"-"}],"services":[{"name":"animal","cost":"10"},{"name":"delivery","cost":"20"}]},{"name":"Яндекс","number":"20-02","type":[{"name":"Эконом","firstkmprice":"29","otherkm":"5"},{"name":"-","firstkmprice":"-","otherkm":"-"},{"name":"Комфорт","firstkmprice":"35","otherkm":"5"},{"name":"-","firstkmprice":"-","otherkm":"-"}],"services":[{"name":"animal","cost":"-"},{"name":"delivery","cost":"-"}]}]';
+    data = '[{"name":"Шара (30 - 40)","number":"30 - 40","type":[{"name":"Эконом","firstkmprice":"25","otherkm":"8"},{"name":"Стандарт","firstkmprice":"49","otherkm":"9"},{"name":"Комфорт","firstkmprice":"59","otherkm":"10"},{"name":"Бизнеc","firstkmprice":"69","otherkm":"10"}],"services":[{"name":"animal","cost":"30"},{"name":"delivery","cost":"10"}]},{"name":"Maxim","number":"75 - 55","type":[{"name":"Эконом","firstkmprice":"35","otherkm":"6"},{"name":"Стандарт","firstkmprice":"-","otherkm":"-"},{"name":"Комфорт","firstkmprice":"50","otherkm":"6.5"},{"name":"Бизнеc","firstkmprice":"50","otherkm":"7.5"}],"services":[{"name":"animal","cost":"10"},{"name":"delivery","cost":"10"}]},{"name":"OnTaxi","number":"2000","type":[{"name":"Эконом","firstkmprice":"29","otherkm":"6"},{"name":"Стандарт","firstkmprice":"45","otherkm":"7"},{"name":"Комфорт","firstkmprice":"55","otherkm":"8"},{"name":"Бизнеc","firstkmprice":"65","otherkm":"11"}],"services":[{"name":"animal","cost":"0"},{"name":"delivery","cost":"0"}]},{"name":"Shark","number":"3000","type":[{"name":"Эконом","firstkmprice":"38.25","otherkm":"5.95"},{"name":"Стандарт","firstkmprice":"45","otherkm":"7"},{"name":"Комфорт","firstkmprice":"58.5","otherkm":"9.1"},{"name":"-","firstkmprice":"-","otherkm":"-"}],"services":[{"name":"animal","cost":"10"},{"name":"delivery","cost":"20"}]},{"name":"Яндекс","number":"20-02","type":[{"name":"Эконом","firstkmprice":"29","otherkm":"5"},{"name":"-","firstkmprice":"-","otherkm":"-"},{"name":"Комфорт","firstkmprice":"35","otherkm":"5"},{"name":"-","firstkmprice":"-","otherkm":"-"}],"services":[{"name":"animal","cost":"0"},{"name":"delivery","cost":"0"}]}]';
 var taxi = JSON.parse(data);
 
 	function calcSharaCost(){
@@ -130,18 +130,15 @@ var taxi = JSON.parse(data);
                 }
                 for (var k = 0; k < serv_inp.length; k++) {
                     if (serv_inp[k].checked){
-                    cost[j][i] += Number(taxi[j].services[k].cost);
+                        if (taxi[j].services[k].cost !== '0') {
+                             cost[j][i] += Number(taxi[j].services[k].cost);
+                        }
+                        else cost[j][i] = "Услуга не доступна"; 
                     }
                 } 
             }	
 		}
-       /*for (var j = 0; j < taxi.length; j++) {
-           for(let i = 0; i < taxi[j].type.length; i++ ){
-            if (cost[j][i]==Infinity) {
-                cost[j][i] = "No price";
-            }
-           }
-       }*/
+       
 		return cost;
 	}
 
@@ -169,11 +166,18 @@ for (var i = 0; i < taxi.length; i++) {
         if (calcSharaCost()[i][radioOn]==0){ 
 
            // continue;
-            rows[i+1].innerHTML = "<div class="+"cell "+ ">" + taxi[i+1].name+ "</div>"+"<div class="+"cell "+">" + taxi[i+1].type[radioOn].name+ "</div>"+"<div class="+"cell"+">" + Math.round(calcSharaCost()[i+1][radioOn]) + 'грн' + "</div>"+"<div class="+"cell"+">" + taxi[i+1].number+ "</div>";
+            rows[i+1].innerHTML = "<div class="+"cell "+ ">" + taxi[i+1].name+ "</div>"+"<div class="+"cell "+">" + taxi[i+1].type[radioOn].name+ "</div>"+"<div class="+"cell"+">" + Math.round(calcSharaCost()[i+1][radioOn]) + ' грн' + "</div>"+"<div class="+"cell"+">" + taxi[i+1].number+ "</div>";
             i+=1;
         }
         else {
-            rows[i+1].innerHTML = "<div class="+"cell "+ ">" + taxi[i].name+ "</div>"+"<div class="+"cell "+">" + taxi[i].type[radioOn].name+ "</div>"+"<div class="+"cell"+">" + Math.round(calcSharaCost()[i][radioOn]) + 'грн' + "</div>"+"<div class="+"cell"+">" + taxi[i].number+ "</div>";
+            rows[i+1].innerHTML = "<div class="+"cell "+ ">" + taxi[i].name+ "</div>"+"<div class="+"cell "+">" + taxi[i].type[radioOn].name+ "</div>"+"<div class="+"cell"+">" + Math.round(calcSharaCost()[i][radioOn]) + ' грн' + "</div>"+"<div class="+"cell"+">" + taxi[i].number+ "</div>";
+        }
+    }
+   var cells = document.getElementsByClassName("cell");
+    for (var i = 0; i < cells.length; i++) {
+        if (cells[i].value == "NaN грн") {
+            console.log("ffff");
+            cells[i].innerHTML = "Услуга отсутствует";
         }
     }
 }
