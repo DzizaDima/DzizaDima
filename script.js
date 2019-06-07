c92f90ed8dd4c67286a1fcef0c32f2077bce428c
DzizaDima <32745918+DzizaDima@users.noreply.github.com>
2019-06-07 14:42:59 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -85,20 +85,20 @@ function computeTotalDistance(result) {
     document.getElementById('total').innerHTML = total;
   
 }
-    data = '[{"name":"Шара (30 - 40)","number":"30 - 40","type":[{"name":"Эконом","firstkmprice":"25","otherkm":"8"},{"name":"Стандарт","firstkmprice":"49","otherkm":"9"},{"name":"Комфорт","firstkmprice":"59","otherkm":"10"},{"name":"Бизнеc","firstkmprice":"69","otherkm":"10"}],"services":[{"name":"animal","cost":"30"},{"name":"delivery","cost":"10"}]},{"name":"Maxim","number":"75 - 55","type":[{"name":"Эконом","firstkmprice":"35","otherkm":"6"},{"name":"Стандарт","firstkmprice":"-","otherkm":"-"},{"name":"Комфорт","firstkmprice":"50","otherkm":"6.5"},{"name":"Бизнеc","firstkmprice":"50","otherkm":"7.5"}],"services":[{"name":"animal","cost":"10"},{"name":"delivery","cost":"10"}]},{"name":"OnTaxi","number":"2000","type":[{"name":"Эконом","firstkmprice":"29","otherkm":"6"},{"name":"Стандарт","firstkmprice":"45","otherkm":"7"},{"name":"Комфорт","firstkmprice":"55","otherkm":"8"},{"name":"Бизнеc","firstkmprice":"65","otherkm":"11"}],"services":[{"name":"animal","cost":"0"},{"name":"delivery","cost":"0"}]},{"name":"Shark","number":"3000","type":[{"name":"Эконом","firstkmprice":"38.25","otherkm":"5.95"},{"name":"Стандарт","firstkmprice":"45","otherkm":"7"},{"name":"Комфорт","firstkmprice":"58.5","otherkm":"9.1"},{"name":"-","firstkmprice":"-","otherkm":"-"}],"services":[{"name":"animal","cost":"10"},{"name":"delivery","cost":"20"}]},{"name":"Яндекс","number":"20-02","type":[{"name":"Эконом","firstkmprice":"29","otherkm":"5"},{"name":"-","firstkmprice":"-","otherkm":"-"},{"name":"Комфорт","firstkmprice":"35","otherkm":"5"},{"name":"-","firstkmprice":"-","otherkm":"-"}],"services":[{"name":"animal","cost":"0"},{"name":"delivery","cost":"0"}]}]';
+    data = '[{"name":"Шара (30 - 40)","number":"30 - 40","type":[{"name":"Эконом","firstkmprice":"25","otherkm":"8"},{"name":"Стандарт","firstkmprice":"49","otherkm":"9"},{"name":"Комфорт","firstkmprice":"59","otherkm":"10"},{"name":"Бизнеc","firstkmprice":"69","otherkm":"10"}],"services":[{"name":"animal","cost":"30"},{"name":"delivery","cost":"10"}]},{"name":"Maxim","number":"75 - 55","type":[{"name":"Эконом","firstkmprice":"35","otherkm":"6"},{"name":"Стандарт","firstkmprice":"-","otherkm":"-"},{"name":"Комфорт","firstkmprice":"50","otherkm":"6.5"},{"name":"Бизнеc","firstkmprice":"50","otherkm":"7.5"}],"services":[{"name":"animal","cost":"10"},{"name":"delivery","cost":"10"}]},{"name":"OnTaxi","number":"2000","type":[{"name":"Эконом","firstkmprice":"29","otherkm":"6"},{"name":"Стандарт","firstkmprice":"45","otherkm":"7"},{"name":"Комфорт","firstkmprice":"55","otherkm":"8"},{"name":"Бизнеc","firstkmprice":"65","otherkm":"11"}],"services":[{"name":"animal","cost":"f"},{"name":"delivery","cost":"f"}]},{"name":"Shark","number":"3000","type":[{"name":"Эконом","firstkmprice":"38.25","otherkm":"5.95"},{"name":"Стандарт","firstkmprice":"45","otherkm":"7"},{"name":"Комфорт","firstkmprice":"58.5","otherkm":"9.1"},{"name":"-","firstkmprice":"-","otherkm":"-"}],"services":[{"name":"animal","cost":"10"},{"name":"delivery","cost":"20"}]},{"name":"Uklon","number":"000000","type":[{"name":"Эконом","firstkmprice":"29","otherkm":"7"},{"name":"Стандарт","firstkmprice":"45","otherkm":"7"},{"name":"Комфорт","firstkmprice":"55","otherkm":"7.5"},{"name":"-","firstkmprice":"-","otherkm":"-"}],"services":[{"name":"animal","cost":"20"},{"name":"delivery","cost":"30"}]},{"name":"Яндекс","number":"20-02","type":[{"name":"Эконом","firstkmprice":"29","otherkm":"5"},{"name":"-","firstkmprice":"-","otherkm":"-"},{"name":"Комфорт","firstkmprice":"35","otherkm":"5"},{"name":"-","firstkmprice":"-","otherkm":"-"}],"services":[{"name":"animal","cost":"f"},{"name":"delivery","cost":"f"}]}]';
 var taxi = JSON.parse(data);
 
-	function calcSharaCost(){
+	function calcCost(){
 		var sharaCost=[];
 		var cost = [];
 		var range = Number(total.innerHTML);
         var serv_inp = document.getElementsByClassName("services");
-       //alert(Number(taxi[0].services[0].cost));
+       
 		for (var j = 0; j < taxi.length; j++) {
 			 cost[j] = [];
              for(let i = 0; i < taxi[j].type.length; i++ ){
-                switch (j){
-                    case 0 : case 2 :  {
+                switch (taxi[j].name){
+                    case "Шара (30 - 40)" : case "OnTaxi" : case "Uklon": {
                     
                         if (range > 1){
                             range - 1;
@@ -109,16 +109,17 @@ var taxi = JSON.parse(data);
                         else {cost[j][i] = taxi[j].type[i].firstkmprice};
                     }
                     break;
-                    case 1 : {
+                    case "Maxim" : {
                         if (range > 2){
                             //range - 2;
                             cost[j][i] = Number(taxi[j].type[i].firstkmprice);
                             cost[j][i] += (range-2)*Number(taxi[j].type[i].otherkm);
                             //cost[l][i] -= cost[i]/100*5;
                         }
+                        else {cost[j][i] = taxi[j].type[i].firstkmprice};
                     }
                     break;
-                    case 3: case 4:{
+                    case "Shark": case "Яндекс": {
                         if (range > 1){
                             range - 1;
                             cost[j][i] = Number(taxi[j].type[i].firstkmprice);
@@ -128,12 +129,15 @@ var taxi = JSON.parse(data);
                         else {cost[j][i] = taxi[j].type[i].firstkmprice};
                     }  
                 }
+                 if (isNumeric(cost[j][i])) {
+           cost[j][i] = Math.round(cost[j][i]);
+        }
                 for (var k = 0; k < serv_inp.length; k++) {
                     if (serv_inp[k].checked){
-                        if (taxi[j].services[k].cost !== '0') {
+                        if (taxi[j].services[k].cost !== 'f') {
                              cost[j][i] += Number(taxi[j].services[k].cost);
                         }
-                        else cost[j][i] = "Услуга не доступна"; 
+                        else cost[j][i] = "Услуга недоступна"; 
                     }
                 } 
             }	
@@ -141,7 +145,17 @@ var taxi = JSON.parse(data);
        
 		return cost;
 	}
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
+function grn(y){
+    var grn = " грн";
+    if(isNumeric(y)){
+        return grn;
+    }
+    else return "";
+}
 function output(){
 	var radio = document.getElementsByName('typecar');
 	for (var i = 0; i < radio.length; i++) {
@@ -160,24 +174,18 @@ for (var i = 0; i < taxi.length; i++) {
     for (var i = 0; i < fillRow+1; i++) {
         table.innerHTML += "<div class="+"row"+">"+"</div>";
     }
+  
     var rows = table.childNodes;
         rows[0].innerHTML = "<div class="+"cell "+ ">" +"Название такси"+ "</div>"+"<div class="+"cell "+">" +"Класс машины"+ "</div>"+"<div class="+"cell"+">" + "Стоимость" + ' (грн)' + "</div>"+"<div class="+"cell"+">" +"Номер"+ "</div>";
     for (var i = 0; i <taxi.length; i++) {
-        if (calcSharaCost()[i][radioOn]==0){ 
-
-           // continue;
-            rows[i+1].innerHTML = "<div class="+"cell "+ ">" + taxi[i+1].name+ "</div>"+"<div class="+"cell "+">" + taxi[i+1].type[radioOn].name+ "</div>"+"<div class="+"cell"+">" + Math.round(calcSharaCost()[i+1][radioOn]) + ' грн' + "</div>"+"<div class="+"cell"+">" + taxi[i+1].number+ "</div>";
+        
+        if (calcCost()[i][radioOn]==0){ 
+            rows[i+1].innerHTML = "<div class="+"cell "+ ">" + taxi[i+1].name+ "</div>"+"<div class="+"cell "+">" + taxi[i+1].type[radioOn].name+ "</div>"+"<div class="+"cell"+">" + calcCost()[i+1][radioOn] + grn(calcCost()[i+1][radioOn]) + "</div>"+"<div class="+"cell"+">" + taxi[i+1].number+ "</div>";
             i+=1;
         }
         else {
-            rows[i+1].innerHTML = "<div class="+"cell "+ ">" + taxi[i].name+ "</div>"+"<div class="+"cell "+">" + taxi[i].type[radioOn].name+ "</div>"+"<div class="+"cell"+">" + Math.round(calcSharaCost()[i][radioOn]) + ' грн' + "</div>"+"<div class="+"cell"+">" + taxi[i].number+ "</div>";
+            rows[i+1].innerHTML = "<div class="+"cell "+ ">" + taxi[i].name+ "</div>"+"<div class="+"cell "+">" + taxi[i].type[radioOn].name+ "</div>"+"<div class="+"cell"+">" + calcCost()[i][radioOn] + grn(calcCost()[i][radioOn]) + "</div>"+"<div class="+"cell"+">" + taxi[i].number+ "</div>";
         }
     }
-   var cells = document.getElementsByClassName("cell");
-    for (var i = 0; i < cells.length; i++) {
-        if (cells[i].value == "NaN грн") {
-            console.log("ffff");
-            cells[i].innerHTML = "Услуга отсутствует";
-        }
-    }
+  
 }
